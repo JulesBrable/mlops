bbd6d178b4ae5000057bc779538eb506c172b066
jbrablx <jules.brable@ensae.fr>
2024-03-05 13:46:01 +0000

    :sparkles: init multipages app

---
+++ Homepage.py
@@ -0,0 +1,24 @@
+"""Streamlit Homepage"""
+
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Paris Events Recommender",
+    page_icon=":tada:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        'Report a bug': "https://github.com/JulesBrable/mlops/issues/new",
+        'About': """
+        If you want to read more about the project, you would be interested in going to the
+        corresponding [GitHub](https://github.com/JulesBrable/mlops) repository.
+
+        Contributions:
+        - [Jules Brablé](linkedin.com/in/jbrable)
+        - [Martin Boutier]()
+        - [Louis Latournerie]()
+
+        """
+    }
+)
--- pages/2_Recommender_Engine.py
+++ pages/2_Recommender_Engine.py
@@ -1,29 +1,9 @@
-"""Streamlit Homepage"""
+"""Recommender Engine"""
 
 import streamlit as st
 
 from src.utils.etl import get_recommendations, show_recommendations
 from src.utils.map import generate_map
-
-st.set_page_config(
-    page_title="Paris Events Recommender",
-    page_icon=":tada:",
-    layout="wide",
-    initial_sidebar_state="expanded",
-    menu_items={
-        'Report a bug': "https://github.com/JulesBrable/mlops/issues/new",
-        'About': """
-        If you want to read more about the project, you would be interested in going to the
-        corresponding [GitHub](https://github.com/JulesBrable/mlops) repository.
-
-        Contributions:
-        - [Jules Brablé](linkedin.com/in/jbrable)
-        - [Martin Boutier]()
-        - [Louis Latournerie]()
-
-        """
-    }
-)
 
 st.columns([1/100, 98/100, 1/100])[1].title(":tada: Paris Events Recommender")
 
