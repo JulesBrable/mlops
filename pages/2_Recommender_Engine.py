f394299ef11ea39607e31b4efcf48ece662cac52
jbrablx <jules.brable@ensae.fr>
2024-03-06 15:50:15 +0000

    add excel download button

--- pages/2_Recommender_Engine.py
+++ pages/2_Recommender_Engine.py
@@ -6,7 +6,7 @@ from langdetect import detect
 from src.utils.etl import preprocess_df, get_recommendations, show_recommendations
 from src.utils.map import generate_map
 from src.utils.translation import lead_text_translator, translate_query
-from src.utils.utils import load_content, make_config, load_style
+from src.utils.utils import load_content, make_config, load_style, to_excel, download_results
 
 
 make_config()
@@ -23,8 +23,8 @@ query = st.sidebar.text_input(
 
 reco_button = st.sidebar.button("Press this to get your event recommendations", type="primary")
 
-if reco_button:
-    if query:
+if 'recommendations' not in st.session_state or reco_button:
+    if query and reco_button:
         with st.spinner(content["spinner_nlp"]):
             source_language = detect(query)
             query_french = translate_query(source_language, query)
@@ -32,9 +32,7 @@ if reco_button:
         st.success('Recommendations Done!')
 
         if (recommendations is not None) and (not recommendations.empty):
-            recommendations = preprocess_df(
-                recommendations, 'Coordonnées géographiques', ["Date de début", "Date de fin"]
-                )
+            recommendations = preprocess_df(recommendations)
             if source_language != 'fr':
                 with st.spinner(content["spinner_translator"]):
                     recommendations = lead_text_translator(
@@ -43,8 +41,12 @@ if reco_button:
                         target=source_language
                         )
                 st.success('Translation Done!')
-
-            show_recommendations(recommendations)
-            generate_map(recommendations)
+            st.session_state.recommendations = recommendations
         else:
             st.markdown("No recommendations found for your query.")
+
+if 'recommendations' in st.session_state and not st.session_state.recommendations.empty:
+    show_recommendations(st.session_state.recommendations)
+    generate_map(st.session_state.recommendations)
+    df_xlsx = to_excel(st.session_state.recommendations)
+    download_results(df_xlsx)
