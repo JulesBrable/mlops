961276d8eca3882e5985402aaf01e7120332f487
mboutier <60610272+mboutier@users.noreply.github.com>
2024-03-07 13:56:28 +0100

    key words wordclouds and bigrams wordcloud on page 2

--- pages/1_Events_Overview.py
+++ pages/1_Events_Overview.py
@@ -1,3 +1,20 @@
 import streamlit as st
+from src.utils.etl import *
+import matplotlib.pyplot as plt
 
-st.markdown("Some descriptive statics here about previous & future events")
+st.title("Dive into the Buzz 🐝")
+st.markdown("### Some descriptive statistics here about previous & future events")
+
+fig, ax = plt.subplots()
+wordcloud = plot_wordclout()
+ax.imshow(wordcloud, interpolation='bilinear')
+ax.axis("off")
+st.pyplot(fig)
+
+
+st.markdown("### Let's look at words associations (bigrams) to inspire you more for your query, and even give you some ideas !")
+fig, ax = plt.subplots()
+wordcloud = plot_wordclout_from_bigrams()
+ax.imshow(wordcloud, interpolation='bilinear')
+ax.axis("off")
+st.pyplot(fig)
--- src/utils/etl.py
+++ src/utils/etl.py
@@ -1,16 +1,19 @@
 """ETL pipelines that are being used in the Streamlit App"""
-
 import streamlit as st
 import pandas as pd
 from dateutil import parser
-
+from wordcloud import WordCloud
+from stop_words import get_stop_words
 from src.models.recommendation import Recommender
-
+from nltk.util import bigrams
+import nltk
+from collections import Counter
 
 @st.cache_data(show_spinner=False)
 def load_data():
     """Loads event data from a specified CSV file hosted online."""
-    df = pd.read_csv("https://minio.lab.sspcloud.fr/jbrablx/mlops/data/raw/data.csv", sep=";")
+    # df = pd.read_csv("https://minio.lab.sspcloud.fr/jbrablx/mlops/data/raw/data.csv", sep=";")
+    df = pd.read_csv("data.csv", sep=";")
     return df
 
 
@@ -21,7 +24,28 @@ def load_recommender():
     recommender = Recommender(df)
     return recommender
 
+@st.cache_resource(show_spinner=False)
+def plot_wordclout():
+    df = load_data()
+    text = " ".join(str(review) for review in df['Mots clés'] if review and not pd.isnull(review))
+    stopwords = set(get_stop_words('french'))
+    wordcloud = WordCloud(stopwords=stopwords, background_color="white", width=800, height=400, mode="RGBA").generate(text)
+    return wordcloud
 
+@st.cache_resource(show_spinner=False)
+def plot_wordclout_from_bigrams():
+    
+    df = load_data()
+    data = df['Mots clés'].astype(str).values.tolist()
+    filtered_data = [item.split(',') for item in data if item != 'nan']
+    
+    bigram_list = [bigram for sublist in filtered_data for bigram in bigrams(sublist)]
+    bigram_strings = ['/'.join(bigram) for bigram in bigram_list]
+    
+    bigram_frequency = Counter(bigram_strings)
+    wordcloud = WordCloud(width=800, height=400, background_color='white').generate_from_frequencies(bigram_frequency)
+    return wordcloud
+    
 def get_recommendations(query):
     """Fetches recommendations based on a user query."""
     recommender = load_recommender()
@@ -47,3 +71,6 @@ def show_recommendations(recommendations):
             to {get_digest_date(row['Date de fin'])}
             """)
             st.write(f"**Plus d'informations en cliquant [ici]({row['URL']}).**")
+            
+            
+
